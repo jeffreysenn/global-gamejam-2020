81748a7147d7c74b06898d49478174cfc2f9a0d7
Lövlie <fredriklovlie@gmail.com>
2020-02-02 11:29:10 +0100

    RespawnTrigger

---
+++ Assets/RespawnTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RespawnTrigger : MonoBehaviour
+{
+    //[SerializeField] private Transform RespawnPoint = null;
+    [SerializeField] private Vector3 RespawnPoint = new Vector3(10, 13);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D pCollision)
+    {
+        if (RespawnPoint == null)
+            return;
+        pCollision.transform.position = RespawnPoint;
+    }
+}
